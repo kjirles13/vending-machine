c8df289537b9c145d324c80a2fc28e786553d6e9
Kit Jirles <kjirles13@gmail.com>
2023-02-09 11:21:25 -0500

    Initialize final variables for MoneyHandler class

--- src/main/java/com/techelevator/MoneyHandler.java
+++ src/main/java/com/techelevator/MoneyHandler.java
@@ -1,4 +1,20 @@
 package com.techelevator;
 
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
 public class MoneyHandler {
+    private final BigDecimal NICKEL = BigDecimal.valueOf(0.05);
+    private final BigDecimal DIME = BigDecimal.valueOf(0.10);
+    private final BigDecimal QUARTER = BigDecimal.valueOf(0.25);
+    private final BigDecimal DOLLAR = BigDecimal.valueOf(1.00);
+
+
+
+
+   /* DecimalFormat df = new DecimalFormat("0.00");
+
+        return df.format(amount.multiply(conversionRate));*/
+
+
 }
