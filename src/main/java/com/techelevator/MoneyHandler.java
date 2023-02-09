64800e8324fc07c7478605d2966ce05503e560f8
Kit Jirles <kjirles13@gmail.com>
2023-02-09 11:27:28 -0500

    Add countMoney method to MoneyHandler class

--- src/main/java/com/techelevator/MoneyHandler.java
+++ src/main/java/com/techelevator/MoneyHandler.java
@@ -3,14 +3,17 @@ package com.techelevator;
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
 
-public class MoneyHandler {
+public abstract class MoneyHandler {
     private final BigDecimal NICKEL = BigDecimal.valueOf(0.05);
     private final BigDecimal DIME = BigDecimal.valueOf(0.10);
     private final BigDecimal QUARTER = BigDecimal.valueOf(0.25);
     private final BigDecimal DOLLAR = BigDecimal.valueOf(1.00);
 
-
-
+    public BigDecimal countMoney(double total, double newAmount) {
+        BigDecimal newTotal = BigDecimal.valueOf(total);
+        newTotal.add(BigDecimal.valueOf(newAmount));
+        return newTotal;
+    }
 
    /* DecimalFormat df = new DecimalFormat("0.00");
 
